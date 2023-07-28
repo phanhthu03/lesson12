84800d84f0887ce24703ae1b34e4edc89665cdd4
phanhthu03 <132880440+phanhthu03@users.noreply.github.com>
2023-07-29 06:54:45 +0700

    Update bai1.php

--- bai1.php
+++ bai1.php
@@ -4,32 +4,40 @@
 
 <?php
 //Kiểm tra thư mục tồn tại chưa có thì tạo mới
-$file = @fopen('data.txt', 'r');
-if (!$file)
-    echo "Mở file không thành công";
-else {
-    echo "mở file thành  công"
-}
+if(file_exists('data.txt'))
+echo 'file tồn tại';
+else
+echo 'file không tồn tại';
 
 //Kiểm tra file có cho phép ghi không
-
-$file = "data.txt";
-if(is_writable($file)) {
-  echo ("$file is writable");
-} else {
-  echo ("$file is not writable");
-}
+if(is_writable('data.txt'))
+    echo 'Được phép ghi';
+else
+    echo 'không được phép ghi';
 
 //Ghi file với nội dung: xin chào 
+    $file = @fopen('data.txt', 'w');
+    if (!$file)
+        echo "Mở file không thành công";
+    else {
+        $data = 'xin chào';
+        fwrite($file, $data);
+    }
+//Đóng file
 $file = @fopen('data.txt', 'w');
 if (!$file)
     echo "Mở file không thành công";
 else {
     $data = 'xin chào';
     fwrite($file, $data);
-    fclose($file);//Đóng file
-    unlink($file);//Xóa file
+
+    fclose($file);
 }
+//Xoá file
+if(unlink('data.txt'))
+    echo 'Thành công';
+else
+    echo 'Không thành công';
 ?>
 </body>
 </html>
